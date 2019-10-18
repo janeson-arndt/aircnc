eb9765456284d7840a00bf153a71c97882dbb444
janeson arndt <janeson.arndt@live.com>
2019-10-18 18:06:44 -0300

    f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
+import api from './services/api';
 import './App.css';
 
-import logo from './assests/logo5.svg';
+import logo from './assests/logo6.svg';
 
 function App() {
+  //funcionamento padrão do formulário quando clica em submit é enviar o usuário para outra tela, a parte de preventDefault é para prever isso
+  function handleSubmit(event) { 
+    event.preventDefault();
+
+    console.log('Hello');
+  }
   return (
     <div className="container">
-      <img src={logo} alt="AirCnc" />
+      <img className="logo" src={logo} alt="AirCnc" />
       <div className="content">
         <p>
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para a sua empresa
     </p>
 
-        <form>
-          <label htmlFor="email">E_MAIL *</label>
+        <form onSubmit = {handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
           <input 
           type="email"  
           id="email" 
           placeholder="Seu melhor e-mail"
           />
-          <button type="submit">Enviar</button>
+          <button className="btn" type="submit">Enviar</button>
         </form>
       </div>
     </div>
