3e431d1d9f73c3ffd2b9b3a4157662010fbdd264
Janeson Arndt <janeson.arndt@gmail.com>
2019-10-23 07:41:55 -0300

    alterado dashboard > index

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -1,18 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react'; //useState para usar o localStorage
 import api from '../../services/api';
 
 export default function Dashboard() {
+    //cria um estado
+    // const [ nomeDoEstado, funcaoParaAtualizarEstado ] = useStage([]), como a info vem do meu back em formato de lista/array é recomendado iniciar com uma lista vazia
+    const [spots, setSpots] = useState([]);
+
     useEffect(() => {
         async function loadSpots() {
             const user_id = localStorage.getItem('user'); // pega o user do locaStorage, conforme localStorage.setItem('user', _id) do Login> index.js
             const response = await api.get('/dashboard', {
                 headers: { user_id }
             })
-            console.log(response.data);
+            // console.log(response.data);
+            setSpots(response.data); //onde estão os dados que vem de nossa API
         }
         loadSpots();
 
-    }, [] ); 
+    }, []);
 
-    return <h1>Dashboard</h1>
+    return (
+        <>
+            <ul className="spot-list">
+
+                {spots.map(spot => ( // percore toda  a lista de spots e retorna um HTML
+                    //tag header em vez da tag img para deixar as imagens pequenas e no mesmo tamanho padrão
+                    //tag strong retorna a empresa
+                    //tag span retorna o preço
+                    <li key={spot._id}>
+                        <header>
+                        </header>
+                        <strong>{spot.company}</strong>
+                        <span>{spot.price}</span>
+                    </li>
+                ))}
+            </ul>
+        </>
+    )
 }
