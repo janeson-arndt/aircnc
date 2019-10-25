5f796f2d446c5e2bbd97fae8dc167b59abf3a66f
Janeson Arndt <janeson.arndt@gmail.com>
2019-10-25 07:38:26 -0300

    criacao New index e style

--- frontend/src/pages/New/index.js
+++ frontend/src/pages/New/index.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import camera from '../../assests/camera.svg';
+
+import './styles.css';
+
 
 export default function New() {
+    const [company, setCompany] = useState('');
+    const [techs, setTechs] = useState('');
+    const [price, setPrice] = useState('');    
+
     function handleSubmit() {
 
     }
 
     return (
         <form onSubmit={handleSubmit}>
+            <label id='thumbnail'>
+                <input type='file'/>
+                <img src={camera} alt="Select img"/>
+                </label>
 
+            <label htmlFor="company">EMPRESA *</label>
+            <input 
+                id='company'
+                placeholder='Sua empresa incrível'
+                value={company}
+                onChange={event => setCompany(event.target.value)}
+            />
+             <label htmlFor="company">TECNOLOGIAS *</label>
+            <input 
+                id='techs'
+                placeholder='Quais tecnologias usam?'
+                value={techs}
+                onChange={event => setTechs(event.target.value)}
+            />
+             <label htmlFor="company">VALOR DA DIÁRIA *<span> (em branco para GRATUÍTO)</span></label>
+            <input 
+                id='price'
+                placeholder='Valor cobrado por dia'
+                value={price}
+                onChange={event => setPrice(event.target.value)}
+            />
+
+            <button type="submit" className='btn'>Cadastrar</button>
         </form>
     
     )
