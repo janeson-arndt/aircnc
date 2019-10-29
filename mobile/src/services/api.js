8c7144d93081e84f2202c8b7fc6b137a59589604
Janeson Arndt <janeson.arndt@gmail.com>
2019-10-29 06:55:30 -0300

    d

--- mobile/src/services/api.js
+++ mobile/src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333', //url localhost funciona apenas no emulador IOS
-    baseURL: 'http://192.168.0.2:3333', //Para funcionar em outros dispositivos na mesma rede, trocar localhost pelo IP da maquina no Expo Developer Tools
+    //baseURL: 'http://localhost:3333', //url localhost funciona apenas no emulador IOS
+   // baseURL: 'http://192.168.0.2:3333', //Para funcionar em outros dispositivos na mesma rede, trocar localhost pelo IP da maquina no Expo Developer Tools
+    baseURL: 'http://192.168.15.11:3333', //Para funcionar em outros dispositivos na mesma rede, trocar localhost pelo IP da maquina no Expo Developer Tools
 
 });
 
